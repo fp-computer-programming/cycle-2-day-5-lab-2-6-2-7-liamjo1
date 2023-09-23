ea4aadd22ef252b89785ce7187d290a32f1fe19e
p24lohara <p24lohara@fairfieldprep.org>
2023-09-23 11:11:12 -0400

    Lab 2-6

--- lab_2-6.py
+++ lab_2-6.py
@@ -1,11 +1,4 @@
-# Create a Python file named lab_2-6.py
-
-# Set a = 5, b = 10, c = 0, d = -4
-
-# Create a conditional statement to determine if each variable is even or odd. 
- 
-# Print the results.
-
+# Liam O'Hara 9/23/2023
 a = 5
 b = 10
 c = 0
