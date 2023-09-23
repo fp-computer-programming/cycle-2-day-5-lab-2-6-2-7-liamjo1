b235f9379093a825c5de586e887903fb81bcc871
p24lohara <p24lohara@fairfieldprep.org>
2023-09-23 11:24:06 -0400

    Lab 2-7

---
+++ lab_2-7.py
@@ -0,0 +1,51 @@
+# Liam O'Hara 9/23/2023
+a = 5
+b = 10
+c = 0
+d = -4
+
+if (a % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
+if a < -5:
+    print("Less than -5")
+if a >= -5 and a <= 5:
+    print("Between -5 and 5")
+else:
+    print("Greater than 5")
+
+if (b % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
+if b < -5:
+    print("Less than -5")
+if b >= -5 and b <= 5:
+    print("Between -5 and 5")
+else:
+    print("Greater than 5")
+
+
+if (c % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
+if c < -5:
+    print("Less than -5")
+if c >= -5 and c <= 5:
+    print("Between -5 and 5")
+else:
+    print("Greater than 5")
+
+
+if (d % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
+if d < -5:
+    print("Less than -5")
+if d >= -5 and d <= 5:
+    print("Between -5 and 5")
+else:
+    print("Greater than 5")
